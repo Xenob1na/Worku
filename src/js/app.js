c608fb4f11979212afe93c75fe31eea760cd96b9
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-04-16 22:38:59 +0500

    commit1

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import * as flsFunctions from "./modules/functions.js";
+import Swiper from 'swiper';
+import {
+	Navigation,
+	EffectFade,
+	Pagination,
+	Thumbs,
+	Controller,
+	FreeMode,
+	Scrollbar,
+	A11y,
+	Mousewheel
+} from 'swiper';
+import AOS from 'aos';
+
+flsFunctions.isWebp();
