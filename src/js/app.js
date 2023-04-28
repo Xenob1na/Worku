f3b5b59075a7438227417ee1ef6fa5e8379c861f
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-04-28 19:01:38 +0700

    Commit5

--- src/js/app.js
+++ src/js/app.js
@@ -53,5 +53,20 @@ window.onclick = function(event) {
 }
 
 
+// Бургерное меню
 
+const navBtn = document.querySelector('.header__icon');
+const nav = document.querySelector('.mobile-nav');
+const fade = document.querySelector('.mobile-nav-fade');
 
+navBtn.onclick = function() {
+  nav.classList.toggle('mobile-nav--open');
+  fade.classList.toggle('mobile-nav-fade-open');
+  document.body.classList.toggle('no-scroll');
+};
+
+fade.onclick = function() {
+  nav.classList.toggle('mobile-nav--open');
+  fade.classList.toggle('mobile-nav-fade--open');
+  document.body.classList.toggle('no-scroll');
+}
