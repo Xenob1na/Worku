e960dce6f1cdfad9bb42f078f8fcfc4bf54a8373
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-04-29 18:45:40 +0700

    Commit7

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import {path} from "./gulp/config/path.js";
 import {plugins} from "./gulp/config/plugins.js";
 
+
 global.app = {
 	isBuild: process.argv.includes('--build'),
 	isDev: !process.argv.includes('--build'),
@@ -9,6 +10,8 @@ global.app = {
 	gulp: gulp,
 	plugins: plugins
 }
+
+
 
 // Имрорт задач
 import {copy} from "./gulp/tasks/copy.js";
